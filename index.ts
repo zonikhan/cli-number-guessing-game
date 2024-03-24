bcc3262c3db81314e3c9a6eb4f6b669e46543e5e
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-03-25 01:43:47 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+ import inquirer from "inquirer";
+  
+
+
+
+
+ const randomNumber =  Math.floor(Math.random() *10 + 1);
+ 
+
+ const answers =await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number beteen 1- 10",
+
+ 
+    
+
+
+ }]);
+ if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you guessed right number.");
+ } else {
+    console.log("you guessed worg number.");
+ }
